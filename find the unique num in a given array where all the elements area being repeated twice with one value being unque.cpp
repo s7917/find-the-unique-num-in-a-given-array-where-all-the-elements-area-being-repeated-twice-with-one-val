1a0b07aae3f7253c17cf832c7467933fc2ca333f
shubham <124447665+s7917@users.noreply.github.com>
2023-06-10 01:03:39 +0530

    Create find the unique num in a given array where all the elements area being repeated twice with one value being unque.cpp

---
+++ find the unique num in a given array where all the elements area being repeated twice with one value being unque.cpp
@@ -0,0 +1,23 @@
+//find the unique num in a given array where all the elements area being repeated twice with one value being unque.
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    int array[]= {2 ,3,1, 3 ,2,4,1};
+    int size = 7;
+    for(int i=0 ; i<size; i++){
+        for(int j=i+1 ; j<size ; j++){
+            if(array[i]==array[j]){
+                array[i]=array[j]=-1;
+            }
+        }
+    }
+
+    for(int i=0 ; i<size ; i++){
+        if(array[i]>0){
+            cout<<array[i]<<endl;
+        }
+    }
+    return 0;
+}
